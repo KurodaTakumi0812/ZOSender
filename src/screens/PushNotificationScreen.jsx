b84c3896fef6c5c5700f7456bf6a73ed346f5d18
だーくろ <63647187+KurodaTakumi0812@users.noreply.github.com>
2021-04-12 17:28:28 +0900

    Update PushNotificationScreen.jsx

--- src/screens/PushNotificationScreen.jsx
+++ src/screens/PushNotificationScreen.jsx
@@ -1,35 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import firebase from 'firebase';
 
 export default function PushNotificationScreen() {
   const [title,setTitle]=useState('');
   const [body,setBody]=useState('');
 
   function handlePress(){
-
+    if (title === ''){
+      Alert.alert('タイトルを入力してください。')
+    }else if (body === ''){
+      Alert.alert('本文を入力してください。')
+    }else{
+      Alert.alert(
+        '確認',
+        'Push通知を送信してもよろしいですか？',
+        [
+          {
+            text: 'cancel',
+            onPress: () => { },
+            style: 'cancel',
+          },
+          {
+            text: 'OK',
+            onPress: ()=>{
+              const db = firebase.firestore();
+              const ref = db.collection('tokens');
+              ref.get().then((doc)=>{
+                doc.forEach((doc)=>{
+                  const data = doc.data();
+                  console.log(data.token)
+                  sendPushNotification(data.token)
+                })
+              })
+            },
+          },
+        ],
+      );
+    }
   };
 
-  // Can use this function below, OR use Expo's Push Notification Tool-> https://expo.io/notifications
-// async function sendPushNotification(expoPushToken) {
-//   const message = {
-//     to: expoPushToken,
-//     sound: 'default',
-//     title: 'Original Title',
-//     body: 'And here is the body!',
-//     data: { someData: 'goes here' },
-//   };
-
-//   await fetch('https://exp.host/--/api/v2/push/send', {
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//       'Accept-encoding': 'gzip, deflate',
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(message),
-//   });
-// }
+async function sendPushNotification(expoPushToken) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: `${title}`,
+    body: `${body}`,
+    data: { someData: 'goes here' },
+  };
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
 
 
   return (
@@ -44,13 +73,12 @@ export default function PushNotificationScreen() {
     <TextInput 
       style={styles.bodyInput} 
       placeholder="本文" 
-      multiline
       value={body}
       onChangeText={text=>setBody(text)}
     />
     <View style={styles.buttonContainer}>
       <Button
-        title="追加"
+        title="送信"
         onPress={handlePress}
       />
     <StatusBar style="auto" />
@@ -77,8 +105,7 @@ const styles = StyleSheet.create({
   bodyInput: {
     marginTop:8,
     width: '90%',
-    height: '60%',
-    textAlignVertical: 'top',
+    height: 38,
     borderWidth: 1,
     backgroundColor: "white",
     paddingHorizontal:4,
